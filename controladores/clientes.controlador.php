994af78dc284581a30759d244dab923610d8586a
Galindo <janthonygalindov@gmail.com>
2022-09-26 19:27:34 -0700

    initial commit

---
+++ controladores/clientes.controlador.php
@@ -0,0 +1,261 @@
+<?php
+
+class ControladorClientes{
+
+	/*=============================================
+	CREAR CLIENTES
+	=============================================*/
+
+	static public function ctrIngresoClientes(){
+		if(isset($_POST["ingCliente"])){
+
+			if(preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $POST["ingUsuario"]) &&
+			   preg_match('/^[a-zA-Z0-9]+$/', $_POST["ingPassword"])){
+
+				$encriptar = crypt($_POST["ingPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+				$tabla = "clientes";
+				
+				$item = "usuario";
+				$valor = $_POST["ingUsuario"];
+
+				$respuesta = ModeloClientes::mdlMostrarClientes($tabla, $item, $valor);
+
+				if($respuesta["email"] == $_POST["ingUsuario"] && $respuesta["password"] == $encriptar){
+					
+					if($respuesta["estado"] == 1){
+
+						$_SESSION["iniciarSesion"] = "ok";
+						$_SESSION["id"] = $respuesta["id"];
+						$_SESSION["nombre"] = $respuesta["nombre"];
+						$_SESSION["email"] = $respuesta["email"];
+						$_SESSION["perfil"] = $respuesta["perfil"];
+
+
+						
+							echo '<script>
+								window.location = "inicio";
+							</script>';
+						
+
+
+					}else{
+
+						echo '<br>
+							<div class="alert alert-danger">El usuario aún no está activado</div>';
+
+					}		
+
+				}
+
+			   }
+		}
+	}
+
+	static public function ctrCrearCliente(){
+
+		if(isset($_POST["nuevoCliente"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["nuevoCliente"]) &&
+			   preg_match('/^[a-zA-Z0-9]+$/', $_POST["nuevoPassword"]) &&
+			   preg_match('/^[0-9]+$/', $_POST["nuevoDocumentoId"]) &&
+			   preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST["nuevoEmail"]) && 
+			   preg_match('/^[()\-0-9 ]+$/', $_POST["nuevoTelefono"]) && 
+			   preg_match('/^[#\.\-a-zA-Z0-9 ]+$/', $_POST["nuevaDireccion"])){
+
+			   	$tabla = "clientes";
+				$encriptar = crypt($_POST["nuevoPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+				$est=1;
+				$per='Cliente';
+
+			   	$datos = array("nombre"=>$_POST["nuevoCliente"],
+							   "password"=> $encriptar,
+					           "documento"=>$_POST["nuevoDocumentoId"],
+					           "email"=>$_POST["nuevoEmail"],
+							   "estado"=>$est,
+							   "perfil"=>$per,
+					           "telefono"=>$_POST["nuevoTelefono"],
+					           "direccion"=>$_POST["nuevaDireccion"],
+					           "fecha_nacimiento"=>$_POST["nuevaFechaNacimiento"]);
+
+			   	$respuesta = ModeloClientes::mdlIngresarCliente($tabla, $datos);
+
+			   	if($respuesta == "ok"){
+
+					echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "El cliente ha sido guardado correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "clientes";
+
+									}
+								})
+
+					</script>';
+
+				}
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "¡El cliente no puede ir vacío o llevar caracteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "clientes";
+
+							}
+						})
+
+			  	</script>';
+
+
+
+			}
+
+		}
+
+	}
+
+	/*=============================================
+	MOSTRAR CLIENTES
+	=============================================*/
+
+	static public function ctrMostrarClientes($item, $valor){
+
+		$tabla = "clientes";
+
+		$respuesta = ModeloClientes::mdlMostrarClientes($tabla, $item, $valor);
+
+		return $respuesta;
+
+	}
+
+	/*=============================================
+	EDITAR CLIENTE
+	=============================================*/
+
+	static public function ctrEditarCliente(){
+
+		if(isset($_POST["editarCliente"])){
+
+			if(preg_match('/^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]+$/', $_POST["editarCliente"]) &&
+			   preg_match('/^[a-zA-Z0-9]+$/', $_POST["editarPassword"]) &&
+			   preg_match('/^[0-9]+$/', $_POST["editarDocumentoId"]) &&
+			   preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/', $_POST["editarEmail"]) && 
+			   preg_match('/^[()\-0-9 ]+$/', $_POST["editarTelefono"]) && 
+			   preg_match('/^[#\.\-a-zA-Z0-9 ]+$/', $_POST["editarDireccion"])){
+
+			   	$tabla = "clientes";
+				$encriptar = crypt($_POST["editarPassword"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+
+			   	$datos = array("id"=>$_POST["idCliente"],
+			   				   "nombre"=>$_POST["editarCliente"],
+							   "password"=>$encriptar,
+					           "documento"=>$_POST["editarDocumentoId"],
+					           "email"=>$_POST["editarEmail"],
+					           "telefono"=>$_POST["editarTelefono"],
+					           "direccion"=>$_POST["editarDireccion"],
+					           "fecha_nacimiento"=>$_POST["editarFechaNacimiento"]);
+
+			   	$respuesta = ModeloClientes::mdlEditarCliente($tabla, $datos);
+
+			   	if($respuesta == "ok"){
+
+					echo'<script>
+
+					swal({
+						  type: "success",
+						  title: "El cliente ha sido cambiado correctamente",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+									if (result.value) {
+
+									window.location = "clientes";
+
+									}
+								})
+
+					</script>';
+
+				}
+
+			}else{
+
+				echo'<script>
+
+					swal({
+						  type: "error",
+						  title: "¡El cliente no puede ir vacío o llevar caracteres especiales!",
+						  showConfirmButton: true,
+						  confirmButtonText: "Cerrar"
+						  }).then(function(result){
+							if (result.value) {
+
+							window.location = "clientes";
+
+							}
+						})
+
+			  	</script>';
+
+
+
+			}
+
+		}
+
+	}
+
+	/*=============================================
+	ELIMINAR CLIENTE
+	=============================================*/
+
+	static public function ctrEliminarCliente(){
+
+		if(isset($_GET["idCliente"])){
+
+			$tabla ="clientes";
+			$datos = $_GET["idCliente"];
+
+			$respuesta = ModeloClientes::mdlEliminarCliente($tabla, $datos);
+
+			if($respuesta == "ok"){
+
+				echo'<script>
+
+				swal({
+					  type: "success",
+					  title: "El cliente ha sido borrado correctamente",
+					  showConfirmButton: true,
+					  confirmButtonText: "Cerrar",
+					  closeOnConfirm: false
+					  }).then(function(result){
+								if (result.value) {
+
+								window.location = "clientes";
+
+								}
+							})
+
+				</script>';
+
+			}		
+
+		}
+
+	}
+
+}
+
---
+++ vistas/modulos/login.php
@@ -0,0 +1,204 @@
+<div id="back"></div>
+
+<div class="login-box">
+  
+  <div class="login-logo">
+
+    <img src="vistas/img/plantilla/logo.farmacia.png" class="img-responsive" style="padding:30px 100px 0px 100px">
+
+  </div>
+
+  <div class="login-box-body">
+
+    <p class="login-box-msg">Ingresar al sistema</p>
+
+    <form method="post">
+
+      <div class="form-group has-feedback">
+
+        <input type="text" class="form-control" placeholder="Usuario" name="ingUsuario" required>
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+
+      </div>
+
+      <div class="form-group has-feedback">
+
+        <input type="password" class="form-control" placeholder="Contraseña" name="ingPassword" required>
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      
+      </div>
+
+      <div class="row">
+       
+        <div class="col-xs-4">
+
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Ingresar</button>
+        
+        </div>
+
+        <div class="col-xs-4">
+
+          <button type="submit" class="btn btn-primary btn-block btn-flat" data-toggle="modal" data-target="#modalAgregarUsuario">Registrarse</button>
+        
+        </div>
+
+      </div>
+
+      <?php
+
+        $login = new ControladorUsuarios();
+        $login -> ctrIngresoUsuario();
+        
+      ?>
+
+    </form>
+
+  </div>
+
+</div>
+
+
+<!--=====================================
+MODAL AGREGAR USUARIO
+======================================-->
+
+<div id="modalAgregarUsuario" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post" enctype="multipart/form-data">
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Agregar usuario</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-user"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Ingresar nombre" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA EL USUARIO -->
+
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-key"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevoUsuario" placeholder="Ingresar usuario" id="nuevoUsuario" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA LA CONTRASEÑA -->
+
+             <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span> 
+
+                <input type="password" class="form-control input-lg" name="nuevoPassword" placeholder="Ingresar contraseña" required>
+
+              </div>
+
+            </div>
+
+            <!-- ENTRADA PARA SELECCIONAR SU PERFIL -->
+
+            <!-- <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-users"></i></span> 
+
+                <select class="form-control input-lg" name="nuevoPerfil">
+                  
+                  <option value="">Selecionar perfil</option>
+
+                  <option value="Administrador">Administrador</option>
+
+                  <option value="Especial">Especial</option>
+
+                  <option value="Vendedor">Vendedor</option>
+
+                  <option value="Cliente">Cliente</option>
+
+                </select>
+
+              </div>
+
+            </div> -->
+
+            <!-- ENTRADA PARA SUBIR FOTO -->
+
+             <div class="form-group">
+              
+              <div class="panel">SUBIR FOTO</div>
+
+              <input type="file" class="nuevaFoto" name="nuevaFoto">
+
+              <p class="help-block">Peso máximo de la foto 2MB</p>
+
+              <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail previsualizar" width="100px">
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar usuario</button>
+
+        </div>
+
+        <?php
+
+          $crearUsuario = new ControladorUsuarios();
+          $crearUsuario -> ctrCrearUsuario();
+
+        ?>
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
