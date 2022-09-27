0ec813ec944a95273e216c737e99439148af40ac
Galindo <janthonygalindov@gmail.com>
2022-09-26 20:57:15 -0700

    cambios footer

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	
-	<strong>Copyright &copy; 2017 <a href="https://www.tutorialesatualcance.com" target="_blank">Tutoriales a tu Alcance</a>.</strong>
+	<strong>Copyright &copy; 2022 <a href="#">Farmacia GLZ, Cumpas Sonora</a>.</strong>
 
 	Todos los derechos reservados.
 
